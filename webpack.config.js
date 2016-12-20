c58a16c2e4d5292a2a49089a959cf2e0a6381ed8
Giovanni Pinto <giovanni.pinto.po@gmail.com>
2016-12-20 14:39:15 +0100

    Added basic webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
+'use strict';
+
+var path = require("path");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const config = {
   entry: './src/app.js',
-  output: './dist/bundle.js'
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devtool: "cheap-eval-source-map",
+  devServer: { contentBase: __dirname + '/dist', inline: true },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'src/index.html'
+  })]
 };
 
 module.exports = config;
